3e67dd4acf7525eb2c4fd62c07839dcb1e7c5c57
serifcolakel <serifcolakel0@gmail.com>
2023-05-20 03:22:59 +0300

    ADDED (serif) : functions definition added.

--- src/utils/generateColorPalette.ts
+++ src/utils/generateColorPalette.ts
@@ -1,4 +1,6 @@
 const expandHexColor = (hexColor: string): string => {
+  // If the hex color is in the abbreviated format (e.g. #ccc),
+  // expand it to the full format (e.g. #cccccc)
   return (
     "#" +
     hexColor[1] +
@@ -14,15 +16,19 @@ function calculateWeightedColorValue(
   colorValue: number,
   weight: number
 ): number {
+  // Divide the color value by 500, then multiply by the weight
+  // to get the weighted color value.
   return Math.round((colorValue * weight) / 500);
 }
 
 function componentToHex(colorComponent: number): string {
+  // Convert a color component to its hex value
   const hex = colorComponent.toString(16);
   return hex.length === 1 ? "0" + hex : hex;
 }
 
 function generateHexColorWithWeight(hexColor: string, weight: number): string {
+  // Given a hex color and a weight, generate a weighted hex color
   const red = parseInt(hexColor.slice(1, 3), 16);
   const green = parseInt(hexColor.slice(3, 5), 16);
   const blue = parseInt(hexColor.slice(5, 7), 16);
@@ -39,6 +45,7 @@ function generateHexColorWithWeight(hexColor: string, weight: number): string {
 }
 
 function isValidHexColor(hexColor: string): boolean {
+  // Check if the hex color is valid
   const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
   return hexColorRegex.test(hexColor);
 }
